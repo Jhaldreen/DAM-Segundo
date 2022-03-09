3bddc19eac5088b79260903c48c1e7a2f7be7c48
Antonio <85558554+Jhaldreen@users.noreply.github.com>
2022-03-09 14:59:13 +0100

    Add files via upload

---
+++ Desarrollo de Interfaces/ExamenEV2_2021/Entregar/Ejercicio6/Ejercicio6/help/help_set.hs
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding='ISO-8859-1' ?>
+<!DOCTYPE helpset
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
+         "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+ 
+
+<helpset version="1.0">
+        <title>Tarea 6</title>
+        <maps>
+                <!-- Pagina por defecto al mostrar la ayuda -->
+                <homeID>aplicacion</homeID>
+                <!-- Que mapa deseamos -->
+                <mapref location="map_file.jhm"/>
+        </maps>
+
+        <!-- Las Vistas que deseamos mostrar en la ayuda -->
+        <view>
+                <name>Tabla Contenidos</name>
+                <label>Tabla de contenidos</label>
+                <type>javax.help.TOCView</type>
+                <data>toc.xml</data>
+        </view>
+
+  <view>
+    <name>Indice</name>
+    <label>El indice</label>
+    <type>javax.help.IndexView</type>
+    <data>indice.xml</data>
+  </view>
+  
+  <view>
+    <name>Buscar</name>
+    <label>Buscar</label>
+    <type>javax.help.SearchView</type>
+    <data engine="com.sun.java.help.search.DefaultSearchEngine">
+      JavaHelpSearch
+    </data>
+  </view>
+  
+</helpset>
+
